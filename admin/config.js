2d0adad1d8adff8c73fce08b05ece282cdaf3bce
Poncmanu <79991522+Poncmanu@users.noreply.github.com>
2026-07-26 07:25:48 -0600

    Delete admin/config.js

--- admin/config.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://TU-PROYECTO.supabase.co";
-const SUPABASE_ANON_KEY = "TU_ANON_KEY";
-
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
