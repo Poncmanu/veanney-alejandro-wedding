8d0805891ccceb517a5237bac061393da69b6181
Poncmanu <79991522+Poncmanu@users.noreply.github.com>
2026-07-26 07:00:15 -0600

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,1 +1,21 @@
-console.log("admin.js cargado");
+document.querySelectorAll(".metric-card").forEach(card=>{
+
+    card.addEventListener("click",()=>{
+
+        document.getElementById("detailTitle").innerHTML=card.innerText;
+
+        document.getElementById("detailContent").innerHTML=`
+
+        <p>Carlos Segura</p>
+
+        <p>Adriana Villagómez</p>
+
+        <p>Alejandro Ruiz</p>
+
+        <p>Ana Lilia Chávez</p>
+
+        `;
+
+    });
+
+});
