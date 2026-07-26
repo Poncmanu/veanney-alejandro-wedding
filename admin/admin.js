ea4ac70f8769b3344efc59059d3dd440ea784c62
Poncmanu <79991522+Poncmanu@users.noreply.github.com>
2026-07-26 07:38:29 -0600

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -17,6 +17,30 @@ async function cargarResumen() {
 
     console.log(data);
 
+document.getElementById("numTotalInvitaciones").textContent = data.total_invitaciones;
+
+document.getElementById("numEnviadas").textContent = data.enviadas;
+
+document.getElementById("numPendientesEnvio").textContent = data.pendientes_envio;
+
+document.getElementById("numConfirmadas").textContent = data.invitaciones_confirmadas;
+
+document.getElementById("numPendientesConfirmacion").textContent = data.pendientes_confirmacion;
+
+document.getElementById("numNoAsistiran").textContent = data.no_asistiran;
+
+document.getElementById("numInvitados").textContent = data.personas_invitadas;
+
+document.getElementById("numPersonasConfirmadas").textContent = data.personas_confirmadas;
+
+document.getElementById("numPersonasPendientes").textContent = data.personas_pendientes;
+
+document.getElementById("numBanquete").textContent = data.banquete;
+
+document.getElementById("numInfantil").textContent = data.infantil;
+
+    
+
 }
 
 cargarResumen();
